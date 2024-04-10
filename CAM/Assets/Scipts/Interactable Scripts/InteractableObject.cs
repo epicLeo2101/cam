fa8a01e03b0d142c77f2f84ffe1bc0d41bd8afdc
epicLeo2101 <91509500+epicLeo2101@users.noreply.github.com>
2024-04-10 10:25:27 -0500

    Doesn't matter Commit

--- CAM/Assets/Scipts/Interactable Scripts/InteractableObject.cs
+++ CAM/Assets/Scipts/Interactable Scripts/InteractableObject.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 
 public class InteractableObject : Interractable
 {
+    private bool Essential = false;
+    private bool Death = false;
+
     private MeshRenderer objectApperance;
+    private Light hoverLight;
 
     private void Start()
     {
+        hoverLight = GetComponentInChildren<Light>();
         objectApperance = GetComponent<MeshRenderer>();
+
+        //hoverLight.enabled = false;
+
+        if (this.tag == "Essential")
+        {
+            Essential = true;
+        }
+
+        if (this.tag == "Death")
+        {
+            Death = true;
+        }
     }
 
     private void Update()
     {
-        if (objectApperance.enabled == true)
+        if (objectApperance.enabled == true && Death == true)
         {
             GetComponent<Collider>().isTrigger = true;
         }
-        if (objectApperance.enabled == false)
+        if (objectApperance.enabled == false && Death == true)
+        {
+            GetComponent<Collider>().isTrigger = false;
+            //hoverLight.enabled = false;
+        }
+
+//---------------------------------------------------------- The one above 'death is the only thing that will run. ----------------------------------------
+
+        if (objectApperance.enabled == false && Essential == true)
+        {
+            GetComponent<Collider>().isTrigger = true;
+            //hoverLight.enabled = false;
+        }
+        if (objectApperance.enabled == true && Essential == true)
         {
             GetComponent<Collider>().isTrigger = false;
         }
@@ -25,6 +55,7 @@ public class InteractableObject : Interractable
 
     public override void OnFocus()
     {
+        hoverLight.enabled = !hoverLight;
         print("Looking at " + gameObject.name);
     }
 
