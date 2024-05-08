c673843ce270b9c1c9257f0defb22b93c0ea5a98
ELHAPPY21 <codbo2freak2345@gmail.com>
2024-05-08 02:24:55 -0500

    Rain and Menu added

--- CAM/Assets/Scipts/MainMenu.cs
+++ CAM/Assets/Scipts/MainMenu.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
-        public void PlayGame ()
+    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
-    
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
